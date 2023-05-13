71e78328b43b16450a487c39eeb981c63755ac89
Heitor Murara <heitormurara@gmail.com>
2023-05-13 16:26:08 -0300

    Create PokeAPIService to request Species Images

---
+++ onTopPoke/Network/Services/PokeAPIService/PokeAPIService.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+enum PokeAPIService {
+    case getImage(fromSpecieId: Int)
+}
+
+extension PokeAPIService: NetworkService {
+    var baseURL: String {
+        switch self {
+        case .getImage:
+            return "https://raw.githubusercontent.com"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case let .getImage(specieId):
+            return "/PokeAPI/sprites/master/sprites/pokemon/\(specieId).png"
+        }
+    }
+    
+    var parameters: [String : Any]? {
+        nil
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getImage:
+            return .get
+        }
+    }
+}
