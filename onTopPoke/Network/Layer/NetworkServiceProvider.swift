f3fc3e34061e5cc536a1ea7ed765b63f4957d336
Heitor Murara <heitormurara@gmail.com>
2023-05-13 15:01:31 -0300

    Refactor request method and separate protocol implementation into extension

--- onTopPoke/Network/Layer/NetworkServiceProvider.swift
+++ onTopPoke/Network/Layer/NetworkServiceProvider.swift
@@ -1,24 +1,27 @@
 import Foundation
 
 protocol NetworkServiceProviding {
-    associatedtype Service: NetworkService
-    
-    func request<E: Decodable>(_ service: Service, completion: @escaping (Result<E, Error>) -> Void)
-}
-
-final class NetworkServiceProvider<Service: NetworkService>: NetworkServiceProviding {
-    private var urlSession: URLSession
-    
-    init(urlSession: URLSession = .shared) {
-        self.urlSession = urlSession
-    }
-    
     /// Uses the Provider's `URLSession` to make the `NetworkService` request, parse the result and completes.
     ///
     /// - Parameters:
     ///     - service: The `NetworkService` that contains the `URLRequest`
     ///     - completion: The completion handler to call when the data task is complete, either with a `Decodable` success or with a `Error` failure.
-    func request<E: Decodable>(_ service: Service, completion: @escaping (Result<E, Error>) -> Void) {
+    func request<E: Decodable>(_ service: NetworkService, completion: @escaping (Result<E, Error>) -> Void)
+}
+
+final class NetworkServiceProvider {
+    private var urlSession: URLSession
+    
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+}
+
+
+// MARK: - NetworkServiceProviding
+
+extension NetworkServiceProvider: NetworkServiceProviding {
+    func request<E: Decodable>(_ service: NetworkService, completion: @escaping (Result<E, Error>) -> Void) {
         urlSession.dataTask(with: service.urlRequest) { (data, _, error) in
             if let error = error {
                 completion(.failure(error))
