a998c04f29c3441eb376c652cda73214f7b97843
Heitor Murara <heitormurara@gmail.com>
2023-05-13 15:35:07 -0300

    Add Regex to recognize the Specie id

--- onTopPoke/Models/PokemonSpecie.swift
+++ onTopPoke/Models/PokemonSpecie.swift
@@ -2,7 +2,19 @@ import Foundation
 
 struct PokemonSpecieListItem: Decodable {
     let name: String
-    let url: URL?
+    private let urlString: String
+    
+    var id: Int? {
+        let range = NSRange(location: 0, length: urlString.utf16.count)
+        let regex = try? NSRegularExpression(pattern: "\\/(\\d+)\\/$")
+        
+        if let match = regex?.firstMatch(in: urlString, range: range),
+           let matchRange = Range(match.range(at: 1), in: urlString),
+           let intValue = Int(urlString[matchRange]) {
+            return intValue
+        }
+        return nil
+    }
     
     enum CodingKeys: CodingKey {
         case name
@@ -12,9 +24,7 @@ struct PokemonSpecieListItem: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        self.name = try container.decode(String.self, forKey: .name)
-        
-        let urlString = try container.decode(String.self, forKey: .url)
-        url = URL(string: urlString)
+        name = try container.decode(String.self, forKey: .name)
+        urlString = try container.decode(String.self, forKey: .url)
     }
 }
