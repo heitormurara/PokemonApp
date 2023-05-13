4e223ee7d063f76fe8ac93cbf7459bd1bd25fd57
Heitor Murara <heitormurara@gmail.com>
2023-05-13 16:57:00 -0300

    Update Pagination Models to store Pages

---
+++ onTopPoke/Network/Models/Page.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct Page {
+    let limit: Int
+    let offset: Int
+    
+    init(from urlString: String) throws {
+        let pattern = #"offset=(\d+)&limit=(\d+)"#
+        do {
+            let regex = try NSRegularExpression(pattern: pattern)
+            let range = NSRange(urlString.startIndex..., in: urlString)
+            
+            guard let match = regex.firstMatch(in: urlString, range: range) else {
+                throw PaginationError.noExactMatchesToRegex
+            }
+            
+            guard let offsetRange = Range(match.range(at: 1), in: urlString),
+                  let offset = Int(urlString[offsetRange]) else {
+                throw PaginationError.invalidOffset
+            }
+            
+            guard let limitRange = Range(match.range(at: 2), in: urlString),
+                  let limit = Int(urlString[limitRange]) else {
+                throw PaginationError.invalidLimit
+            }
+            
+            self.offset = offset
+            self.limit = limit
+        } catch let error {
+            throw error
+        }
+    }
+}
+
+enum PaginationError: Error {
+    case invalidLimit
+    case invalidOffset
+    case noExactMatchesToRegex
+}
