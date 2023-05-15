4959e06de5fef9f9313546eb8387bf931f0708b2
Heitor Murara <heitormurara@gmail.com>
2023-05-14 22:14:35 -0300

    Add Routes for getSpecie and getEvolutionChain

--- onTopPoke/Network/Services/Pokemon/PokemonRoute.swift
+++ onTopPoke/Network/Services/Pokemon/PokemonRoute.swift
@@ -1,11 +1,13 @@
 enum PokemonRoute {
     case getSpecies(_ page: Page)
+    case getSpecie(fromSpecieId: Int)
+    case getEvolutionChain(fromChainId: Int)
 }
 
 extension PokemonRoute: NetworkRoute {
     var baseURL: String {
         switch self {
-        case .getSpecies:
+        case .getSpecies, .getSpecie, .getEvolutionChain:
             return "https://pokeapi.co"
         }
     }
@@ -14,6 +16,10 @@ extension PokemonRoute: NetworkRoute {
         switch self {
         case .getSpecies:
             return "/api/v2/pokemon-species"
+        case let .getSpecie(specieId):
+            return "/api/v2/pokemon-species/\(specieId)"
+        case let .getEvolutionChain(chainId):
+            return "/api/v2/evolution-chain/\(chainId)"
         }
     }
     
@@ -21,12 +27,14 @@ extension PokemonRoute: NetworkRoute {
         switch self {
         case let .getSpecies(page):
             return ["limit": String(page.limit), "offset": String(page.offset)]
+        case .getSpecie, .getEvolutionChain:
+            return nil
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .getSpecies:
+        case .getSpecies, .getSpecie, .getEvolutionChain:
             return .get
         }
     }
